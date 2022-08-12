1f2938a9ebaba6f8bca9afbbd4c7b8b99395d4f0
bcostaaa01 <bcostinha01@gmail.com>
2022-08-12 15:20:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-q9n6be
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-q9n6be)
---
+++ src/components/Boomerang.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<h1>Data from JSON Placeholder API</h1>
+{{result}}
+</div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  setup () {
+    const result = ref(null)
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(response => response.json())
+    .then(data => result.value = data)
+    return {result}
+  }
+}
+</script>
